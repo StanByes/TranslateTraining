ec31a54bde75f8238224dcefc1a55666a93ac889
StanByes <stanbyes@gmail.com>
2024-02-29 22:54:58 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import createError, { HttpError } from "http-errors";
+import path from "path";
+const translate = require("translatte");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/generate", async (req, res) => {
+    res.contentType("json");
+
+    const { count } = req.query;
+    if (!count)
+        return res.status(400).send("Bad request");
+
+    const words = await fetch(`https://random-word-api.herokuapp.com/word?lang=es&number=${count}`, {method: "GET"});
+    const jsonWords: string[] = await words.json();
+
+    const result: any[] = [];
+    for (let word of jsonWords) {
+        const translation = await translate(word, {
+            from: "es",
+            to: "fr"
+        });
+        result.push({
+            base: word,
+            translate: translation.text
+        });
+    }
+
+    return res.status(200).send(result);
+})
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function(err: HttpError, req: Request, res: Response) {
+    res.status(err.statusCode);
+    return res.send(`${req.method} ${req.originalUrl} [${err.statusCode}] ${err.message}`);
+});
+
+app.listen(3000, () => {
+    console.log("App launched");
+});
---
+++ src/public/js/main.js
@@ -0,0 +1,55 @@
+let word = {base: "", translate: ""};
+
+const informations = document.getElementById("informations");
+const loading = document.getElementById("loading");
+const newButton = document.getElementById("new");
+const translate = document.getElementById("translate");
+const validate = document.getElementById("validate");
+const wordLabel = document.getElementById("word");
+
+function validateWord() {
+    translate.setAttribute("disabled", true);
+    validate.classList.add("disabled");
+
+    const text = translate.value.toLowerCase();
+    if (text == word.translate.toLowerCase()) {
+        informations.classList.add("text-success");
+        informations.innerHTML = "Correct !";
+    } else {
+        informations.classList.add("text-danger");
+        informations.innerHTML = `Faux ! La traduction du mot était ${word.translate.toLowerCase()}.`;
+    }
+}
+
+async function newWord() {
+    informations.classList.remove("text-danger");
+    informations.classList.remove("text-success");
+    informations.innerHTML = "";
+
+    loading.classList.remove("d-none");
+    loading.classList.add("d-block");
+    newButton.classList.add("disabled");
+    translate.setAttribute("disabled", true);
+    translate.value = "";
+    validate.classList.add("disabled");
+    wordLabel.innerHTML = "Loading...";
+
+    const words = await (await fetch("/generate?count=1", {method: "GET"})).json();
+    word = words[0];
+    console.log(word);
+
+    loading.classList.add("d-none");
+    loading.classList.remove("d-block");
+    newButton.classList.remove("disabled");
+    translate.removeAttribute("disabled");
+    validate.classList.remove("disabled");
+    wordLabel.innerHTML = word.base.toLowerCase();
+}
+
+translate.onkeydown = (ev) => {
+    if (ev.key == "Enter")
+        validateWord();
+}
+validate.onclick = validateWord;
+
+newWord();
